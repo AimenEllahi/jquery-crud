bc0b78df4319b65ead4214d71170445d3247825c
AimenEllahi <98141911+AimenEllahi@users.noreply.github.com>
2022-04-24 10:43:34 +0500

    minor changes

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
+  //calling functions here
     loadProducts();
     $(".product-section").on("click", ".delete-btn", handleDelete);
+    $(".product-section").on("click", ".edit-btn", handleUpdate);
     $("#add").click(addProduct);
+    $("#save-changes").click(function(evt) {   //save changes
+      var id = $("#updateId").val();
+      var title = $(".UpdateInputName").val();
+      var desc = $(".UpdateInputDesc").val();
+      var price = $(".UpdateInputCost").val();
+     
+
+      $.ajax({
+       url: "http://localhost:3001/products/" + id,
+       data: { title, desc, price },
+       method: "PATCH",
+       success: function(response) {
+         console.log(response);
+         loadProducts();
+         $("#edit-form").modal("hide");
+       }
+     });
+   });
   });
 
 
 
-
+//function to load products
 const loadProducts = function() {
     $.ajax({
-        url:  "http://localhost:3000/products",
+        url:  "http://localhost:3001/products",
         method: "GET",
         error: function(response) {
            var div = $(".product-section");
@@ -21,22 +41,17 @@ const loadProducts = function() {
               div.empty();
               for(var i =0; i < response.length; i++){
                   div.append(
-                    `<div class="col-lg-3 col-md-5 col-10 productCard p-0 mt-5" data-id="${response[i]._id}">
-                     <img class = "product-img" src ="./images/shampoo.jpg">
-                    <div class="product-image-container p-4"  style="color: black;">
-                        <h2 class="title" >${response[i].productname}</h2>
-                        <span class="price">${response[i].productcost}</span>
+                    `<div class="col-lg-4 col-md-6 col-10 p-5 mt-5" data-id="${response[i]._id}">
+                    <div class = "productCard"> 
+                    <img class = "product-img" src ="./images/shampoo.jpg">
+                    <div class = "p-4 product-details">
+                    <h2 class="title" >${response[i].title}</h2>
+                        <span class="price">${response[i].price}</span>
                         <span class="desc">${response[i].desc}</span>
-                        <div class="stars">
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star"></span>
-                            <span class="fa fa-star"></span>
-                        </div>
-                        <button class="own-btn edit-btn">Edit</button>
+                        <button class="own-btn edit-btn"  data-toggle="modal" data-target="#edit-form">Edit</button>
                         <button class="own-btn delete-btn">Delete</button>
                     </div>
+                </div>
                 </div>`
                   )
               }
@@ -46,13 +61,14 @@ const loadProducts = function() {
       });
   }
 
+//function to delete product  
   function handleDelete() {
     var btn = $(this);
     var parentDiv = btn.closest(".productCard");
     let id = parentDiv.attr("data-id");
     console.log(id);
     $.ajax({
-      url: "http://localhost:3000/products/" + id,
+      url: "http://localhost:3001/products/" + id,
       method: "DELETE",
       success: function() {
         loadProducts();
@@ -60,7 +76,7 @@ const loadProducts = function() {
     });
   }
 
-
+//create product function
   function addProduct() {
     var title = $(".inputName").val();
     var desc = $(".inputDesc").val();
@@ -68,7 +84,7 @@ const loadProducts = function() {
 
     console.log(title,desc, price)
     $.ajax({
-      url: "http://localhost:3000/products",
+      url: "http://localhost:3001/products",
       method: "POST",
       data: { title, desc, price},
       success: function(response) {
@@ -79,6 +95,24 @@ const loadProducts = function() {
         $("#create").modal("hide");
       }
     });
-    
+  }  
   
+//handle update function
+  function handleUpdate() {
+    var btn = $(this);
+    var parentDiv = btn.closest(".productCard");
+    let id = parentDiv.attr("data-id");
+    $.get("http://localhost:3001/products/" + id, function(
+      response
+    ) {
+      console.log(response);
+      $("#updateId").val(response._id);
+      $(".UpdateInputName").val(response.title);  //
+      $(".UpdateInputDesc").val(response.desc);
+      $(".UpdateInputCost").val(response.price);
+
+    });
   }
+
+
+  
